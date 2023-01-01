414575a684f07f9a24cadf6dfe2723ba9cc75043
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-01-01 21:33:57 +0100

    updateFiles

--- TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/GestionnaireCompte.java
+++ TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/GestionnaireCompte.java
@@ -4,10 +4,27 @@
  */
 package ma.emsi.tpbanquenouhaila.ejb;
 
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+
 /**
  *
  * @author Pc
  */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="2622", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
 public class GestionnaireCompte {
     
 }
