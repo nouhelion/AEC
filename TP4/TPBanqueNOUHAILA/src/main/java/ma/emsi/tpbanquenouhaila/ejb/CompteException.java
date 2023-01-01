3ba4ecb3c56069ff18347f270d3bc58123a2c1a8
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-01-01 23:02:31 +0100

    Update CompteException.java

--- TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/CompteException.java
+++ TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/CompteException.java
@@ -8,6 +8,17 @@ package ma.emsi.tpbanquenouhaila.ejb;
  *
  * @author Pc
  */
-public class CompteException {
-    
+public class CompteException extends Exception {
+
+  public CompteException(String message) {
+    super(message);
+  }
+
+  public CompteException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public CompteException(Throwable cause) {
+    super(cause);
+  }
 }
