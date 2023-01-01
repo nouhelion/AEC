cb6c7d8a2a8f5a9d671b2e3321e264da73d1207e
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-01-01 21:41:23 +0100

    update

--- TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/GestionnaireCompte.java
+++ TP4/TPBanqueNOUHAILA/src/main/java/ma/emsi/tpbanquenouhaila/ejb/GestionnaireCompte.java
@@ -6,25 +6,42 @@ package ma.emsi.tpbanquenouhaila.ejb;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import ma.emsi.tpbanquenouhaila.entities.CompteBancaire;
 
 /**
  *
  * @author Pc
  */
-@DataSourceDefinition (
-    className="com.mysql.cj.jdbc.MysqlDataSource",
-    name="java:app/jdbc/banque",
-    serverName="localhost",
-    portNumber=3306,
-    user="root",              // nom et
-    password="2622", // mot de passe que vous avez donnés lors de la création de la base de données
-    databaseName="banque",
-    properties = {
-      "useSSL=false",
-      "allowPublicKeyRetrieval=true"
-    }
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "2622", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true"
+        }
 )
 @Stateless
 public class GestionnaireCompte {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void creerCompte(CompteBancaire compte) {
+        em.persist(compte);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery query
+                = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+        return query.getResultList();
+    }
 }
