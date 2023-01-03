5839c54dcc2966f56a5d0162ece05208a218c294
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-01-03 20:20:26 +0100

    redoProject

---
+++ TP4/tpbanque/src/main/java/ma/emsi/tpbanquenouhaila/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package ma.emsi.tpbanquenouhaila;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Pc
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ TP4/tpbanque/src/main/java/ma/emsi/tpbanquenouhaila/entities/CompteBancaire.java
@@ -0,0 +1,106 @@
+package ma.emsi.tpbanquenouhaila.entities;
+
+import ma.emsi.tpbanquenouhaila.ejb.CompteException;
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+
+/**
+ * Un compte bancaire.
+ *
+ * @author Pc
+ */
+@NamedQuery(name = "CompteBancaire.findAll", query = "select c from CompteBancaire c")
+@Entity
+public class CompteBancaire implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String nom;
+  private int solde;
+
+  public CompteBancaire(String nom, int solde) {
+    this.nom = nom;
+    this.solde = solde;
+  }
+
+  public CompteBancaire() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Get the value of nom
+   *
+   * @return the value of nom
+   */
+  public String getNom() {
+    return nom;
+  }
+
+  /**
+   * Set the value of nom
+   *
+   * @param nom new value of nom
+   */
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public int getSolde() {
+    return solde;
+  }
+
+  public void setSolde(int solde) {
+    this.solde = solde;
+  }
+
+  public void deposer(int montant) {
+    solde += montant;
+  }
+
+  public void retirer(int montant) throws CompteException {
+    if (montant <= solde) {
+      solde -= montant;
+    } else {
+      throw new CompteException("Solde du compte de " + this.nom 
+              + " est " + solde
+              + " ; insuffisant pour un retrait de " + montant);
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object
+  ) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof CompteBancaire)) {
+      return false;
+    }
+    CompteBancaire other = (CompteBancaire) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "fr.grin.tpbanque.entities.CompteBancaire[ id=" + id + " ]";
+  }
+
+}
