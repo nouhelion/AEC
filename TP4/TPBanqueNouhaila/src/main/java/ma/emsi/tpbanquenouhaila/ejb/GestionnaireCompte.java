478beefe090da071f931a2ee9b1c2e65748f3cb2
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-01-09 00:31:07 +0100

    persistance+GestionnaireCompte

---
+++ TP4/TPBanqueNouhaila/src/main/java/ma/emsi/tpbanquenouhaila/ejb/GestionnaireCompte.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tpbanquenouhaila.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author Pc
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "2622", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true"
+        }
+)
+@Stateless
+public class GestionnaireCompte {
+
+}
